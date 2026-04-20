6ea37ef14741ad53f080513a7eb54909f8ebc149
Wellington Pimentel da Silva <wpimentel@sessp.gov.br>
2026-04-20 17:03:34 -0300

    feat - criação do Middleware com as exceções e exibição dos erros

---
+++ GerenciamentoVeiculos/GerenciamentoVeiculos/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Text.Json;
+using GerenciamentoVeiculos.Exceptions;
+
+namespace GerenciamentoVeiculos.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private RequestDelegate _next;
+        
+        public ExceptionMiddleware (RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+
+                var statusCode = ex switch
+                {
+                    NaoEncontradoException => (int)HttpStatusCode.NotFound,
+                    ValidacaoException => (int)HttpStatusCode.BadRequest,
+                    RegraNegocioException => (int)HttpStatusCode.InternalServerError
+
+                };
+
+                context.Response.StatusCode = statusCode;
+
+                var response = new
+                {
+                    erro = ex.Message,
+                    statusCode
+                };
+
+                await context.Response.WriteAsync (JsonSerializer.Serialize (response));
+            }
+        }
+
+    }
+}
