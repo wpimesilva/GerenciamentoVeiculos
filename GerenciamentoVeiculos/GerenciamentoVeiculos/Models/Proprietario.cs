e3451062d0e8c810bbdf3498772d5b336b9804f2
Wellington Pimentel da Silva <wpimentel@sessp.gov.br>
2026-04-20 17:16:36 -0300

    feat - adição de validação nos models (obrigatoriedade e regras)

--- GerenciamentoVeiculos/GerenciamentoVeiculos/Models/Proprietario.cs
+++ GerenciamentoVeiculos/GerenciamentoVeiculos/Models/Proprietario.cs
@@ -1,10 +1,18 @@
-﻿namespace GerenciamentoVeiculos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciamentoVeiculos.Models
 {
     public class Proprietario
     {
+       
         public Guid Id { get; set; } = Guid.NewGuid();
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage="nome deve ter entre 3 e 100 caracteres.")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Cpf é obrigatório")] 
+        [RegularExpression(@"^d\{11}$", ErrorMessage = "Cpf deve conter 11 numeros.")]
         public string Cpf { get; set; }
+        [Required(ErrorMessage = "Data de nascimento é obrigatório")] 
         public DateTime DataNascimento { get; set; }
         public bool Ativo { get; set; } = true;
     }
--- GerenciamentoVeiculos/GerenciamentoVeiculos/Models/Veiculo.cs
+++ GerenciamentoVeiculos/GerenciamentoVeiculos/Models/Veiculo.cs
@@ -1,11 +1,18 @@
-﻿namespace GerenciamentoVeiculos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciamentoVeiculos.Models
 {
     public class Veiculo
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+        [Required(ErrorMessage = "Placa  é obrigatório")]
+        [RegularExpression(@"^[A-Z]{3}-\d{4}$", ErrorMessage = "Placa deve ter o formato ABC-1234")]
         public string Placa { get; set; }
+        [Required(ErrorMessage = "Modelo  é obrigatório")]
         public string Modelo { get; set; }
+        [Range(1981, int.MaxValue, ErrorMessage = "Ano deve ser maior que 1980")]
         public int Ano { get; set; }
+        [Required(ErrorMessage = "ProprietarioId é obrigatório")] 
         public Guid ProprietarioId { get; set; }
         public bool Ativo { get; set; } = true;
 
