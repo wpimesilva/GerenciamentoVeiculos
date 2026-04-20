e036a69def1134a59fffd374050b6e8baee29cdb
Wellington Pimentel da Silva <wpimentel@sessp.gov.br>
2026-04-20 17:51:11 -0300

    feat - adição do repository do veiculo

---
+++ GerenciamentoVeiculos/GerenciamentoVeiculos/Repositories/VeiculoRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GerenciamentoVeiculos.Data;
+using GerenciamentoVeiculos.Models;
+using GerenciamentoVeiculos.Repositories.Interfaces;
+
+namespace GerenciamentoVeiculos.Repositories;
+
+
+public class VeiculoRepository : IVeiculosRepository
+{
+    private readonly AppDbContext _context;
+    public VeiculoRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<Veiculo>> ObterTodosAsync() =>
+       await _context.Veiculos
+       .Where(p => p.Ativo)
+       .AsNoTracking()
+       .ToListAsync();
+
+    public async Task<Veiculo?> ObterPorIdAsync(Guid Id) =>
+        await _context.Veiculos
+        .FirstOrDefaultAsync(p => p.Ativo && p.Id == Id);
+
+    public async Task<Veiculo?> ObterPorPlacaAsync(string Placa) =>
+        await _context.Veiculos
+        .FirstOrDefaultAsync(p => p.Ativo && p.Placa == Placa);
+
+    public async Task AdicionarAsync(Veiculo veiculo)
+    {
+        _context.Add(veiculo);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AtualizaAsync(Veiculo veiculo)
+    {
+        _context.Update(veiculo);
+        await _context.SaveChangesAsync();
+    }
+
+
+}
