40d94eceb9e714c6ed0fc28f2488622de2118923
Wellington Pimentel da Silva <wpimentel@sessp.gov.br>
2026-04-20 16:20:13 -0300

    feat - criação do model Proprietário

---
+++ GerenciamentoVeiculos/GerenciamentoVeiculos/Models/Proprietario.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciamentoVeiculos.Models
+{
+    public class Proprietario
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; } = true;
+    }
+}
