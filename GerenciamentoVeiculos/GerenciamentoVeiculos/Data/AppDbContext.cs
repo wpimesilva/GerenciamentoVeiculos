8de9c6a68d48d4a0a69455cbda5b8ccfe410867a
Wellington Pimentel da Silva <wpimentel@sessp.gov.br>
2026-04-20 16:45:54 -0300

    feat - configuração do AppDbContext

---
+++ GerenciamentoVeiculos/GerenciamentoVeiculos/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GerenciamentoVeiculos.Models;
+
+namespace GerenciamentoVeiculos.Data;
+
+public class AppDbContext : DbContext
+
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+    public DbSet<Proprietario> Proprietarios => Set<Proprietario>();
+    public DbSet<Veiculo> Veiculos => Set<Veiculo>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Proprietario>()
+            .HasMany(p=> p.Veiculos)
+            .WithOne(v=> v.Proprietarios)
+            .HasForeignKey(v => v.ProprietarioId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- GerenciamentoVeiculos/GerenciamentoVeiculos/Program.cs
+++ GerenciamentoVeiculos/GerenciamentoVeiculos/Program.cs
@@ -1,3 +1,6 @@
+using GerenciamentoVeiculos.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
